ec4b4f533ef17060736ffb4f04416dfe4078e61d
adiharel <adi.harel10@gmail.com>
2025-05-04 09:41:25 +0300

    temporary commit to sync

---
+++ src/hooks/useCategoryMetadata.ts
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCategories, Category } from '../api/categoryApi';
+
+export const useCategories = () => {
+    useQuery<Category[]>({
+        queryKey: ['categories'],
+        queryFn: getCategories
+    });
+}
---
+++ src/pages/ListingDetailsPage.tsx
@@ -0,0 +1,11 @@
+import {
+    Box, Button, Card, CardContent, CardMedia, CircularProgress,
+    Typography, Stack
+} from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { formatDistanceToNow } from 'date-fns';
+import { useListing } from '../hooks/useListing';
+
+export default function ListingDetailPage() {
+    
+}
