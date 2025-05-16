cebc7362da2c4b127927f0d71cee2e7f8a14baa0
Adi Harel <adi.harel@nice.com>
2025-05-16 17:31:52 +0300

    fix possible bug

--- src/hooks/useCreateBid.ts
+++ src/hooks/useCreateBid.ts
@@ -8,17 +8,17 @@ export const useCreateBid = (listingId: string) => {
 
     return useMutation({
         mutationFn: (payload: Omit<CreateBidPayload, 'listingId'>) =>
-            createBid({listingId, ...payload}), // Make sure createBid returns a Promise
+            createBid({listingId, ...payload}),
         onSuccess: () => {
             enqueueSnackbar('Bid placed!', {variant: 'success'});
             queryClient.invalidateQueries({queryKey: ['listings', listingId]});
             queryClient.invalidateQueries({queryKey: ['bids', listingId]});
         },
-        onError: (error: AxiosError) => {
-            const msg =
-                error?.response?.data?.cause ??
-                'Bid failed - please try  again';
-            enqueueSnackbar(msg, {variant: 'error'});
+        onError: (error: AxiosError<{ cause?: string }>) => {
+                const msg =
+                    error?.response?.data?.cause ??
+                    'Bid failed - please try again';
+                enqueueSnackbar(msg, {variant: 'error'});
         },
     });
 }
